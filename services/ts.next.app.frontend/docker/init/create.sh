a20d06dc615341122d6b2c2fa48934d39243dc0d
egoma <webeng.perilla@gmail.com>
2024-04-10 19:20:28 +0900

    シェルスクリプトのDC_SERRVICE_NAME変数の修正 #27

--- services/ts.next.app.frontend/docker/init/create.sh
+++ services/ts.next.app.frontend/docker/init/create.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Docker Compose サービス名
-DC_SERRVICE_NAME='ts.next.app.frontend'
-
 cd `dirname $0`
+source ../../.service_env
+
 cd ../../../../
 
 source .env
@@ -13,6 +12,7 @@ mkdir -p ./services/${DC_SERRVICE_NAME}/src
 # https://ja.react.dev/learn/typescript
 # https://nextjs.org/docs/getting-started/installation
 # https://nextjs.org/docs/pages/api-reference/create-next-app
+# npx create-next-app@latest . --src-dir --ts --eslint --tailwind --app --import-alias '@/*'
 docker compose run -u $(id -u):$(id -g) --rm ${DC_SERRVICE_NAME} bash -c "
 npx create-next-app@latest .
 "
